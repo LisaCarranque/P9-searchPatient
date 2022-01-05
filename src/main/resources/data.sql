9a36e0698959477e94bd01987bb789f5bef90fae
LisaCarranque <lisacarranquerios@protonmail.com>
2022-01-05 17:30:05 +0100

    udpate model with enum gender

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,22 @@
 
 use P9;
-insert ignore into patient (uuid, address, birthdate, genre, phone, firstname, lastname) values
+insert ignore into patient (uuid, address, birthdate, gender, phone, firstname, lastname) values
   ('cd23fd55-d832-4890-b5b6-0a7a63987c14', '1509 Culver St', '1980-10-02', 'F', '841-874-6512', 'leonie', 'ki');
 commit;
-insert ignore into patient (uuid, address, birthdate, genre, phone, firstname, lastname) values
+insert ignore into patient (uuid, address, birthdate, gender, phone, firstname, lastname) values
   ('33e814e4-0f8f-4fb3-a65d-6dd4908d16bf', '1519 Culver St', '1980-10-02', 'M', '841-874-6512', 'leon', 'ki');
 commit;
-insert ignore into patient (uuid, address, birthdate, genre, phone, firstname, lastname) values
+insert ignore into patient (uuid, address, birthdate, gender, phone, firstname, lastname) values
   ('cd23fd55-d832-4890-b5b6-0a7a63987c12', '1519 Culver St', '1981-10-02', 'M', '841-874-6512', 'john', 'ki');
 commit;
 
 
-insert ignore into patient (uuid, address, birthdate, genre, phone, firstname, lastname) values
+insert ignore into patient (uuid, address, birthdate, gender, phone, firstname, lastname) values
   ('', '1509 Culver St', '2000-10-02', 'F', '841-874-6512', 'lou', 'smith');
 commit;
-insert ignore into patient (uuid, address, birthdate, genre, phone, firstname, lastname) values
+insert ignore into patient (uuid, address, birthdate, gender, phone, firstname, lastname) values
   ('', '1519 Culver St', '2000-10-02', 'M', '841-874-6512', 'lila', 'smith');
 commit;
-insert ignore into patient (uuid, address, birthdate, genre, phone, firstname, lastname) values
+insert ignore into patient (uuid, address, birthdate, gender, phone, firstname, lastname) values
   ('', '1519 Culver St', '2000-10-02', 'M', '841-874-6512', 'lili', 'smith');
 commit;
